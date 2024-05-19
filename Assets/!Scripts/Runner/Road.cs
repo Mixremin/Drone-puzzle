35e39282859702b06184a9407e252e34fa3fc85c
vilkassl <violetta.pisakova@gmail.com>
2024-05-19 15:03:39 +0300

    TestRunner level
    Test version of runner level with spawner system.

---
+++ Assets/!Scripts/Runner/Road.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+
+    [SerializeField] private float _speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        DestroyRoad();
+    }
+
+    private void FixedUpdate()
+    {
+
+        Move();
+
+    }
+
+    private void Move()
+    {
+
+        transform.Translate(-transform.forward * _speed * Time.fixedDeltaTime);
+
+    }
+
+    private void DestroyRoad()
+    {
+
+        if (transform.position.z < -20)
+            Destroy(gameObject);
+
+    }
+
+}
---
+++ Assets/!Scripts/Runner/RoadSpawner.cs
@@ -0,0 +1,37 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawner : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> _roads;
+    [SerializeField] private float _roadLength = 10;
+    private GameObject _road;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        _road = Instantiate(_roads[Random.Range(0, _roads.Count - 1)], transform.position, Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    public void Spawn()
+    {
+
+        Vector3 position = new Vector3(0, 0, _road.transform.position.z + _roadLength);
+        _road = Instantiate(_roads[Random.Range(0, _roads.Count - 1)], position, Quaternion.identity);
+
+    }
+
+}
