1e152b89b7c8703e43fa115e8dbf05c86500279c
Mixremin <omega8428@gmail.com>
2024-05-20 16:12:24 +0300

    TextMesh pro and Test of Interaction

---
+++ Assets/!Scripts/Items/Phone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace _Items
+{
+    [AddComponentMenu("Scripts/_Items/_Items.Phone")]
+    internal class Phone : MonoBehaviour, IInteractable
+    {
+        [SerializeField]
+        private TextMeshProUGUI itsPhoneText;
+
+        [TextAreaAttribute]
+        [SerializeField]
+        private string interactedField;
+
+        [SerializeField]
+        private float holdTime = 3f;
+        public void Interact()
+        {
+            _ = StartCoroutine(showText());
+        }
+
+        private IEnumerator showText()
+        {
+            itsPhoneText.text = interactedField;
+            itsPhoneText.enabled = true;
+            yield return new WaitForSeconds(holdTime);
+            itsPhoneText.enabled = false;
+        }
+    }
+}
