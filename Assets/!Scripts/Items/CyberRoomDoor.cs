fa2b6601f643f3a253b6f150b5dd6ed059bcb88c
Mixremin <omega8428@gmail.com>
2024-05-21 16:13:37 +0300

    Room with all interactions

---
+++ Assets/!Scripts/Items/CyberRoomDoor.cs
@@ -0,0 +1,37 @@
+﻿using _Config;
+using System; 
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Items
+{
+    [AddComponentMenu("Scripts/_Items/_Items.Door")]
+    internal class CyberRoomDoor : MonoBehaviour, IInteractable
+    {
+        [SerializeField]
+        private int newSceneNum;
+
+        [SerializeField]
+        private ShowTextInteraction needPhoneText;
+
+        [SerializeField]
+        private ShowTextInteraction needDroneText;
+
+        public void Interact()
+        {
+            if (SimpleInventory.instance.hasDrone && SimpleInventory.instance.phoneUsed)
+            {
+                SceneManager.LoadScene(newSceneNum);
+            } 
+            else if (!SimpleInventory.instance.phoneUsed)
+            {
+                needPhoneText.Interact();
+            } else if (!SimpleInventory.instance.hasDrone)
+            {
+                needDroneText.Interact();
+            }
+
+
+        }
+    }
+}
---
+++ Assets/!Scripts/Items/Radio.cs
@@ -0,0 +1,89 @@
+﻿using System; 
+using UnityEngine;
+
+namespace _Items
+{
+    [AddComponentMenu("Scripts/_Items/_Items.Radio")]
+    internal class Radio : MonoBehaviour, IInteractable
+    {
+        internal enum RadioType
+        {
+            turnedOff,
+            music1,
+            music2,
+            music3,
+            noise,
+            threat
+        }
+
+
+        [Header("Sound")]
+        [SerializeField]
+        private AudioSource radioSound;
+
+        [Header("Music")]
+
+        [SerializeField]
+        private AudioClip music1;
+
+        [SerializeField]
+        private AudioClip music2;
+
+        [SerializeField]
+        private AudioClip music3;
+
+        [SerializeField]
+        private AudioClip noise;
+
+        [SerializeField]
+        private AudioClip threat;
+
+        [Header("Mode")]
+        [SerializeField]
+        private RadioType radioType = RadioType.turnedOff;
+
+        public void Interact()
+        {
+            PlayMusic();
+        }
+
+        private void PlayMusic()
+        {
+            switch (radioType)
+            {
+                case RadioType.turnedOff:
+                    radioType = RadioType.music1;
+                    ChangeMusic(music1);
+                    break;
+                case RadioType.music1:
+                    radioType = RadioType.music2;
+                    ChangeMusic(music2);
+                    break;
+                case RadioType.music2:
+                    radioType = RadioType.music3;
+                    ChangeMusic(music3);
+                    break;
+                case RadioType.music3:
+                    radioType = RadioType.noise;
+                    ChangeMusic(noise);
+                    break;
+                case RadioType.noise:
+                    radioType = RadioType.threat;
+                    ChangeMusic(threat);
+                    break;
+                case RadioType.threat:
+                    radioType = RadioType.turnedOff;
+                    radioSound.Stop();
+                    break;
+
+            }
+        }
+
+        private void ChangeMusic(AudioClip clip)
+        {
+            radioSound.clip = clip;
+            radioSound.loop = true;
+            radioSound.Play();
+        }
+    }
+}
