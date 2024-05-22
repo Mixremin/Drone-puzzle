856f90c211591bbd52c5b0ad58e96746752f99f9
Mixremin <omega8428@gmail.com>
2024-05-23 02:36:14 +0300

    Underground passage

---
+++ Assets/!Scripts/LevelSpecific/GoToUnderground.cs
@@ -0,0 +1,21 @@
+﻿using _Config;
+using _Items;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _LevelSpecific
+{
+    [AddComponentMenu("Scripts/_LevelSpecific/_LevelSpecific.GoToUnderground")]
+    internal class GoToUnderground : MonoBehaviour, IInteractable
+    {
+        [SerializeField]
+        private int undergroundScene = 5;
+        public void Interact()
+        {
+            if (SimpleInventory.instance.runnerPazzlePassed)
+            {
+                SceneManager.LoadScene(undergroundScene);
+            }
+        }
+    }
+}
