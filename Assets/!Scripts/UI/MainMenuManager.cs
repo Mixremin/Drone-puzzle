9ea5c3d4c7d2bafbd96e7fc095b5ae303af88b5e
Mixremin <omega8428@gmail.com>
2024-05-26 14:33:13 +0300

    New Main Menu

---
+++ Assets/!Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _UI
+{
+    [AddComponentMenu("Scripts/_UI/_UI.MainMenuManager")]
+    internal class MainMenuManager : MonoBehaviour
+    {
+        [Header("Windows")]
+        [SerializeField]
+        private GameObject mainMenu;
+        [SerializeField]
+        private GameObject settings;
+        [SerializeField]
+        private GameObject soundsSettings;
+        [SerializeField]
+        private GameObject controlSettings;
+
+        private void Start()
+        {
+            OpenMainMenu();
+        }
+
+        public void OpenMainMenu()
+        {
+            mainMenu.SetActive(true);
+            settings.SetActive(false);
+        }
+
+        public void OpenSoundSettings()
+        {
+            mainMenu.SetActive(false);
+            settings.SetActive(true);
+            soundsSettings.SetActive(true);
+            controlSettings.SetActive(false);
+        }
+
+        public void OpenControlSettings()
+        {
+            mainMenu.SetActive(false);
+            settings.SetActive(true);
+            soundsSettings.SetActive(false);
+            controlSettings.SetActive(true);
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+
+        public void StartGame()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
